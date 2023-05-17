b9d2e88e474479d24035925321a8c483993f0bea
6ara6ac <106093131+6ara6ac@users.noreply.github.com>
2023-05-17 11:35:38 +0000

    Deploying to gh-pages from @ 6ara6ac/school2tradeNew@9a73368be7e11af8ded425d959403cf4dae493cf 🚀

---
+++ index.ee94521f.js
@@ -0,0 +1,2 @@
+const c={langActive:document.querySelectorAll(".icon-active"),langInactive:document.querySelectorAll(".icon-inactive")};function e(){c.langInactive.forEach((c=>c.classList.toggle("is-hidden")))}c.langActive.forEach((c=>c.addEventListener("click",e)));
+//# sourceMappingURL=index.ee94521f.js.map
