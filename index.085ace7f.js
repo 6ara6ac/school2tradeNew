1a2fc14b672fe4922fbc303e6237588a89f6c0fb
6ara6ac <106093131+6ara6ac@users.noreply.github.com>
2023-05-24 12:37:33 +0000

    Deploying to gh-pages from @ 6ara6ac/school2tradeNew@c63dfea50ef8a2ea95e603e7a20ce3b4e0255ad5 🚀

--- index.085ace7f.js
+++
@@ -1,2 +0,0 @@
-!function(){function e(){var e=window.pageXOffset?window.pageXOffset:document.documentElement.scrollLeft?document.documentElement.scrollLeft:document.body.scrollLeft;document.getElementById("header").style.left=-e+"px"}window.onscroll=e,window.onresize=e}();
-//# sourceMappingURL=index.085ace7f.js.map
